e55ceb6d3dfd980012cde22dfe521d98ed051f16
Josh Long <josh@joshlong.com>
2019-10-10 02:24:37 -0500

    lets try MongoDB

--- src/main/kotlin/com/example/app/AppApplication.kt
+++ src/main/kotlin/com/example/app/AppApplication.kt
@@ -1,17 +1,17 @@
 package com.example.app
 
+import org.springframework.boot.ApplicationRunner
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 import org.springframework.context.support.beans
 import org.springframework.data.annotation.Id
 import org.springframework.data.repository.reactive.ReactiveCrudRepository
 import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken
-import org.springframework.security.oauth2.core.oidc.user.DefaultOidcUser
 import org.springframework.security.oauth2.core.oidc.user.OidcUser
 import org.springframework.web.reactive.function.server.ServerResponse
 import org.springframework.web.reactive.function.server.body
 import org.springframework.web.reactive.function.server.router
-import reactor.core.publisher.Mono
+import reactor.core.publisher.Flux
 
 @SpringBootApplication
 class AppApplication
@@ -20,10 +20,21 @@ fun main(args: Array<String>) {
     runApplication<AppApplication>(*args) {
         val context = beans {
             bean {
+                val repository = ref<CustomerRepository>()
+                ApplicationRunner {
+                    Flux
+                            .just("James", "Matt", "Josh")
+                            .map { Customer(name = it) }
+                            .flatMap { repository.save(it) }
+                            .thenMany(repository.findAll())
+                            .subscribe { println(it) }
+                }
+            }
+            bean {
                 val repo = ref<CustomerRepository>()
                 router {
                     GET("/hello") {
-                        val reply  = it
+                        val reply = it
                                 .principal()
                                 .map { it as OAuth2AuthenticationToken }
                                 .map { it.principal as OidcUser }
@@ -35,12 +46,12 @@ fun main(args: Array<String>) {
                     }
                 }
             }
-
         }
         addInitializers(context)
     }
 }
 
+
 interface CustomerRepository : ReactiveCrudRepository<Customer, Integer>
 
-data class Customer(@Id val id: Integer, val name: String)
+data class Customer(@Id var id: String? = null, val name: String)
